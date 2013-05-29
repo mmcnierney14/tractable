6234d62c41da8cfd30bd1a1f15688dc7d79a0b34
Diana Salsbury <diana.salsbury@gmail.com>
2013-05-28 21:17:20 -0400

    some formatting for the metrics page

--- src/edu/dartmouth/cs/tractable/MetricsTabFragment.java
+++ src/edu/dartmouth/cs/tractable/MetricsTabFragment.java
@@ -46,6 +46,8 @@ public class MetricsTabFragment extends Fragment {
 
 		String av_experience;
 		String num_sessions;
+		String fave_bathroom = "";
+		String most_used_bathroom = "";
 
 		double total_meals = 0;
 		double total_sleep = 0;
@@ -56,6 +58,8 @@ public class MetricsTabFragment extends Fragment {
 			av_experience = String
 					.valueOf(get_av_experience(mBathroomSessionCursor)); 
 			num_sessions = String.valueOf(get_total_sessions(mBathroomSessionCursor));
+			fave_bathroom = get_fave_bathroom(mBathroomSessionCursor);
+			//most_used_bathroom = get_most_used_bathroom(mBathroomSessionCursor);
 			// if no entries have been logged, av = none
 			try {
 				total_meals = readMealFile("meals.txt");
@@ -72,6 +76,8 @@ public class MetricsTabFragment extends Fragment {
 		} else {
 			av_experience = "None Logged Yet!";
 			num_sessions = "0";
+			fave_bathroom = "None Logged Yet!";
+			most_used_bathroom = "None Logged Yet!";
 		}
 		DecimalFormat df = new DecimalFormat("0.00##");
 		String result_meals = df.format(meals);
@@ -96,7 +102,16 @@ public class MetricsTabFragment extends Fragment {
 		TextView average_sleep = (TextView) view
 				.findViewById(R.id.av_hours_sleep_text);
 		average_sleep.setText(result_sleep);
-
+		
+		// set the favorite bathroom text in xml
+//		TextView favorite_bathroom = (TextView) view
+//				.findViewById(R.id.favorite_bathroom_text);
+//		favorite_bathroom.setText(fave_bathroom);
+//		
+//		// set the most used bathroom text in xml
+//		TextView frequency_bathroom = (TextView) view
+//				.findViewById(R.id.most_used_bathroom_text);
+//		frequency_bathroom.setText(most_used_bathroom);
 		return view;
 
 	}
@@ -122,33 +137,41 @@ public class MetricsTabFragment extends Fragment {
 		return av_experience;
 	}
 	
+	
 	// function to get the most used bathroom
 	public String get_fave_bathroom(Cursor cursor) {
 		// move cursor to beginning of set
 		cursor.moveToFirst();
 		
 		String result = "";
+
+		String favorite = " ";
+		int index = mBathroomSessionCursor.getColumnIndex(Globals.KEY_BUILDING);
 		
 		// find max used bathroom string
 		do {
-//			String cur_bathroom = cursor.getString(Globals.KEY_BUILDING);
-//	        int counter = 0;
-//	        for (int i = 1; i < num.length; ++i) {
-//	            if (current_number == Integer.parseInt(num[i])) {
-//	                ++counter;
-//
-//	            } else if (counter == 0) {
-//	                current_number = Integer.parseInt(num[i]);
-//	                ++counter;
-//
-//	            } else {
-//	                --counter;
-//
-//	            }
-//	        }
-//
-//	        return current_number;
+			int count = 1, tempCount;
+			String temp = "";
+			
+	        for (int i = 1; i < cursor.getCount(); i++) {
+	        	tempCount = 0;
+	        	for (int j = 1; j < cursor.getCount(); j++) {
+	        		cursor.moveToPosition(j);
+	        		String current = cursor.getString(index);
+	        		if (temp.equalsIgnoreCase(current)) {
+	        			tempCount ++;
+	        		}
+	        	}
+	        	cursor.moveToPosition(i);
+	        	if (tempCount > count) {
+	        		favorite = temp;
+	        		count = tempCount;
+	        	}
+	        }
+
 		} while (cursor.moveToNext());
+		
+		result = favorite;
 		
 		return result;
 	}
